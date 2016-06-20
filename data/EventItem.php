e169e38aa26e82fdc77c6692705c416f5a578265
Angel Guevara <aguevara@tecnocen.com>
2016-06-20 13:40:23 -0500

    active calendar from data provider

---
+++ data/EventItem.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace tecnocen\yearcalendar\data;
+
+use Date;
+use yii\web\JsExpression;
+
+/**
+ * Interface to provide the structure for the items in the `dataSource` option
+ * for bootstra-year-calendar plugin.
+ *
+ * @author Angel (Faryshta) Guevara <aguevara@tecnocen.com>
+ */
+interface EventItem extends \yii\base\Arrayable
+{
+    /**
+     * Gets the `endDate` property of a `dataSource` item.
+     *
+     * @return JsExpression containing a js `Date` object
+     */
+    public function geCalendarEventtId();
+
+    /**
+     * Gets the `name` property of the `dataSource` item.
+     *
+     * @return string
+     */
+    public function getTitle();
+
+    /**
+     * Gets the `startDate` property of a `dataSource` item.
+     *
+     * @return JsExpression containing a js `Date` object
+     */
+    public function getStart();
+}
---
+++ widgets/ActiveFullcalendar.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace tecnocen\yearcalendar\widgets;
+
+use yii\data\DataProviderInterface;
+use tecnocen\yearcalendar\data\EventItem;
+
+class ActiveFullcalendar extends Calendar
+{
+    /**
+     * @var DataProviderInterface the data provider for the view. This property
+     * is required.
+     */
+    public $dataProvider;
+
+    /**
+     * @var callable replaces the functionality of `prepareItem()` method.
+     * the signature must be
+     * ```php
+     * function (DataSourceItem $model, $key, $inde, $widget)
+     * ```
+     *
+     * - `$model`: the current data model being rendered
+     * - `$key`: the key value associated with the current data model
+     * - `$index`: the zero-based index of the data model in the model array returned by [[dataProvider]]
+     * - `$widget`: the DataSourceCalendar object
+     */
+    public $prepareItem;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->dataProvider === null
+            || !$this->dataProvider instanceof DataProviderInterface
+        ) {
+            throw new InvalidConfigException(
+                'The "dataProvider" property must be set.'
+            );
+        }
+
+        parent::init();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        $this->prepareDataSource();
+        return parent::run();
+    }
+
+    public function prepareDataSource()
+    {
+        $models = array_values($this->dataProvider->getModels());
+        $keys = $this->dataProvider->getKeys();
+        $rows = [];
+        foreach ($models as $index => $model) {
+            $key = $keys[$index];
+
+            $rows[] = $this->prepareItem($model, $key, $index);
+        }
+
+        $this->clientOptions['dataSource'] = $rows;
+    }
+
+    public function prepareItem(EventItem $model, $key, $index)
+    {
+        if ($this->prepareItem !== null) {
+            return call_user_func(
+                $this->prepareItem,
+                $model,
+                $key,
+                $index,
+                $this
+            );
+        }
+
+        $item = $model->toArray();
+        $item['id'] = $model->geCalendarEventId();
+        $item['title'] = $model->getTitle();
+        $item['start'] = $model->getStart();
+        return $item;
+    }
+}
