0e97889e3fc7b1583b44e653393ba78fa351d81f
Angel Guevara <aguevara@tecnocen.com>
2016-06-20 13:40:41 -0500

    ajax calendar with json feed

---
+++ widgets/AjaxFullcalenda.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace tecnocen\fullcalendar\widgets;
+
+use yii\base\InvalidConfigException;
+use yii\helpers\Url;
+
+/**
+ * @author Angel (Faryshta) Guevara <aguevara@tecnocen.com>
+ */
+class AjaxFullcalenda extends Fullcalendar
+{
+    /**
+     * @var mixed Property to be passed to the `Url::top()` helper method to
+     * created the events feed.
+     */
+    public $url;
+
+    /**
+     * @var array list of sources to be used as event feed. Each element must
+     * contain an 'url' index.
+     */
+    public $sources;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if (isset($this->url) && isset($this->sources)) {
+            throw new InvalidConfigException(
+                'At least one of `$url` or `$sources` properties must be defined.'
+            );
+        }
+
+        if (!isset($this->url)) {
+            $this->clientOptions['events'] = Url::to($this->url);
+        } else {
+            foreach ($this->sources as $source) {
+                if (!isset($source['url'])) {
+                    throw new InvalidConfigException(
+                        'Each element in `$source` must contain an "url" index.'
+                    );
+                }
+                $source['url'] = Url::to($source['url']);
+                $this->clientOptions['eventSources'] = $source;
+            }
+        }
+    }
+}
